eec738386d2e35f3497495acdfa39d90690f74fb
BoomBoomMushroom <dillioncat7@gmail.com>
2024-02-24 20:02:04 -0500

    No Navigation! YIPPEE

--- ProxyVerse/app/src/androidTest/java/com/example/proxyverse/ExampleInstrumentedTest.java
+++ ProxyVerse/app/src/androidTest/java/com/example/proxyverse/ExampleInstrumentedTest.java
@@ -23,7 +23,4 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.proxyverse", appContext.getPackageName());
     }
-
-
-
 }
--- ProxyVerse/settings.gradle.kts
+++ ProxyVerse/settings.gradle.kts
@@ -15,4 +15,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "ProxyVerse"
 include(":app")
- 
